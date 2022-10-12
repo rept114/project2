f63c87031edc464d278196f150991e4b97fe39fb
Fer <reikafer@gmail.com>
2022-10-12 08:09:11 -0700

    mensaje

--- project2/CeldaBiografiaController.swift
+++ project2/CeldaBiografiaController.swift
@@ -3,7 +3,7 @@
 import Foundation
 import UIKit
 class CeldaBiografiaController: UITableViewCell {
-    @IBOutlet weak var lblName: UILabel!
     @IBOutlet weak var lblBiografia: UILabel!
+    
     
 }
